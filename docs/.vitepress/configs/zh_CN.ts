e1a3c8b50f9b8ebc72a6e775b1765e1edbedffd1
竹语林声 <ZhuYuLinSheng@outlook.com>
2025-10-26 14:14:56 +0800

    首页更新

--- docs/.vitepress/configs/zh_CN.ts
+++ docs/.vitepress/configs/zh_CN.ts
@@ -2,15 +2,15 @@ import { defineConfig, type DefaultTheme } from 'vitepress'
 import {  nav, sidebar, } from './'
 
 export const zh_CN = defineConfig({
-    title: 'MCJPG组织网站',
-    description: 'Minecraft服务器交流组织 (MCJPG)-一个致力于Minecraft技术交流和服务器宣传的新兴组织',
+    title: 'MCCOM组织网站',
+    description: 'Minecraft服务器交流组织 (MCCOM)-一个致力于Minecraft技术交流和服务器宣传的新兴组织',
 
     themeConfig: {
-        siteTitle: 'MCJPG组织',
+        siteTitle: 'MCCOM组织',
         nav,
         sidebar,
         editLink: {
-            pattern: 'https://github.com/MineJPGcraft/MCJPG/edit/main/docs/:path',
+            pattern: 'https://github.com/zhuyulinsheng/mccom/edit/main/docs/:path',
             text: '编辑此页面'
           },
         
@@ -20,8 +20,8 @@ export const zh_CN = defineConfig({
           },
       
         footer: {
-            message: 'MCJPG服务器交流组织',
-            copyright: 'Copyright © 2025-present <a href="https://mcjpg.org/">MCJPG</a>— —<a href="https://github.com/MineJPGcraft/MCJPG">网站的仓库</a>'
+            message: 'MCCOM服务器交流组织',
+            copyright: 'Copyright © 2025-present <a href="https://mccom.xyz/">MCCOM</a>— —<a href="https://github.com/zhuyulinsheng/mccom">网站的仓库</a>'
           },
       
         lastUpdated: {
