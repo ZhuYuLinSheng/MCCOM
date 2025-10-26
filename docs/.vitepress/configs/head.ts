59191a8981c9adbcfd59477cb675cea6ce145ada
竹语林声 <ZhuYuLinSheng@outlook.com>
2025-10-26 14:37:16 +0800

    网站标题栏图片更新

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -4,8 +4,8 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  ['link', { rel: 'apple-touch-icon', href: '/logo.png' }],
-  ['link', { rel: 'mask-icon', href: '/logo.png', color: '#3eaf7c' }],
-  ['meta', { name: 'msapplication-TileImage', content: '/logo.png' }],
+  ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
+  ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
+  ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
 ]
