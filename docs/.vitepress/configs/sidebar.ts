699b7cfd9de011d490966e5e291081f26dbe390d
竹语林声 <ZhuYuLinSheng@outlook.com>
2025-10-29 17:41:18 +0800

    专栏大更新

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -9,11 +9,11 @@ export const sidebar = {
             base: '/press/特别栏目/',
             collapsed: false,
             items: [
-                {text: '2024国庆特辑', link: '2024国庆特辑/'},
-                {text: '2025新年特辑', link: '2025新年特辑/'},
-                {text: '一周年特辑', link: '一周年特辑/'},
-                {text: '2025国庆特辑', link: '2025国庆特辑/'},
-                {text: '论坛合作', link: '论坛合作/'},
+                {text: '成立特辑', link: '成立特辑/'},
+                //{text: '2025新年特辑', link: '2025新年特辑/'},
+                //{text: '一周年特辑', link: '一周年特辑/'},
+                //{text: '2025国庆特辑', link: '2025国庆特辑/'},
+                //{text: '论坛合作', link: '论坛合作/'},
             ]
           },
           {
